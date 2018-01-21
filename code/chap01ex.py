0f7e617a0fff0e13164a8291a18840c9eedeb581
Hank Ehly <henry.ehly@gmail.com>
2018-01-21 18:27:01 +0900

    Chapter 1 exercises

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -12,6 +12,7 @@ import sys
 
 import nsfg
 import thinkstats2
+import thinkplot
 
 
 def main(script):
@@ -19,6 +20,28 @@ def main(script):
 
     script: string script name
     """
+
+    dct = thinkstats2.ReadStataDct('2002FemResp.dct')
+    df = dct.ReadFixedWidth('2002FemResp.dat.gz', compression='gzip')
+    nsfg.CleanFemResp(df)
+
+    dct2 = thinkstats2.ReadStataDct('2002FemPreg.dct')
+    df2 = dct2.ReadFixedWidth('2002FemPreg.dat.gz', compression='gzip')
+    nsfg.CleanFemPreg(df2)
+
+    hist1 = thinkstats2.Hist(df['pregnum'].values)
+    hist2 = thinkstats2.Hist(df2['pregnum'].values)
+
+    thinkplot.subplot(1, 1, 2)
+    thinkplot.Hist(hist1)
+
+    thinkplot.subplot(2, 1, 2)
+    thinkplot.Hist(hist2)
+
+    thinkplot.show()
+
+    # print(df['pregnum'].value_counts().sort_index(), df2['pregnum'].value_counts().sort_index())
+
     print('%s: All tests passed.' % script)
 
 
